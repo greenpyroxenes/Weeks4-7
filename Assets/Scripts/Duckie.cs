ffb9750c8fc2650d9d222674f219458796a1211a
Athena Mclarney <mclarney@shernet.sheridancollege.ca>
2026-02-03 10:01:24 -0500

    Made noise button and move on mouse

---
+++ Assets/Scripts/Duckie.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Duckie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(Mouse.current.leftButton.wasPressedThisFrame == true && EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            transform.position = mousePos;
+        }
+    }
+}
