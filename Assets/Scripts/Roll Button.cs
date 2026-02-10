4b0cabeff671a5261cb1beec63892aaa68cdfadc
MSI\rober <greenpyroxene@gmail.com>
2026-02-09 21:38:40 -0500

    Started to figure out how to change pips visibility on launch and randomize with button

---
+++ Assets/Scripts/Roll Button.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RollButton : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int setDie()
+    {
+        int die = 0;
+        die = Random.Range(0, 6);
+        return die;
+    }
+
+    public void getDie()
+    {
+        int roll = setDie();
+        if(roll == 1)
+        {
+            
+        }
+    }
+}
